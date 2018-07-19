4e856f6cd37625df963b471be4d85584aad7ff37
Weinan Li <l.weinan@gmail.com>
2018-07-20 01:26:59 +0800

    .

---
+++ src/main/java/io/weli/pili/PiliApplication.java
@@ -0,0 +1,12 @@
+package io.weli.pili;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiliApplication.class, args);
+	}
+}
