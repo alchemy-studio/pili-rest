d54691b88c506cf295b73138205d954a66349603
Weinan Li <l.weinan@gmail.com>
2018-07-22 18:31:40 +0800

    .

--- src/main/java/io/weli/pili/search/PiliEmojiRepo.java
+++ src/main/java/io/weli/pili/search/PiliEmojiRepo.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public interface PiliEmojiRepo extends SolrCrudRepository<PiliEmoji, String> {
 
-    @Query("name:*?0*")
+    @Query("name:?0")
     List<PiliEmoji> findAllByName(String keyword);
 }
 
